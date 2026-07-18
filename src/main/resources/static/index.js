970a44e8df1a349bf3b13c2ce0b85436004ed82e
Iahana Kundaliya <ishanakundaliya@gmail.com>
2026-07-18 12:48:47 +0530

    random user implemented

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -43,3 +43,34 @@ function toggleUser(){
     userGender.innerHTML = users[id].gender;
 
 }
+
+function randomUser(){
+    fetch("https://randomuser.me/api/")
+        .then(function(response){
+            //convert raw text to jason
+           return  response.json();
+
+        }) 
+        .then(function(data){
+
+         var userData=data.results[0];
+          
+             //1: image 
+         var userImage = document.getElementById("user-image");
+         userImage.src = userData.picture.large;
+ 
+            //2: name
+         var userName = document.getElementById("user-name");
+         userName.innerHTML= userData.name.first+ " "+
+                                 userData.name.last;
+
+            //3: gender
+         var userGender = document.getElementById("user-gender");
+          userGender.innerHTML = userData.gender;
+
+         
+        })
+        .catch(function(err){
+            console.log(err);
+        })
+}
